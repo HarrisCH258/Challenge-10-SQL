6dd1b797c47ddb0fb1a6d727a09db9d7118c6274
CH258 <ch2953har@gmail.com>
2024-10-27 18:45:43 -0400

    add tables to schema

---
+++ database/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS inventory_db;
+
+CREATE DATABASE inventory_db;
+
+\c inventory_db;
+
+CREATE TABLE Department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE Role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+);
+
+CREATE TABLE Employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+);
